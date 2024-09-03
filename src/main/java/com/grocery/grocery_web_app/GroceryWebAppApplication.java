16289b96761c3bad7070fcf4bbae7b4bb184a687
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-03 16:30:34 +0530

    initial commit

---
+++ src/main/java/com/grocery/grocery_web_app/GroceryWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.grocery.grocery_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryWebAppApplication.class, args);
+		System.out.println("success...");
+	}
+
+}
