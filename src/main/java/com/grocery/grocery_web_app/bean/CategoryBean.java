602ead741923f6e2704e4f83b0be67030f0da9e0
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-04 09:29:57 +0530

    initial commit

---
+++ src/main/java/com/grocery/grocery_web_app/bean/CategoryBean.java
@@ -0,0 +1,13 @@
+package com.grocery.grocery_web_app.bean;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @ToString
+public class CategoryBean {
+	private int categoryId;
+	private String categoryName;
+	private String categoryVisible;
+}
