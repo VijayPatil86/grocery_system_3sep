8d7aebaed54893bbb7c158905cda299a81d9c1c5
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-04 09:29:04 +0530

    initial commit

---
+++ src/main/java/com/grocery/grocery_web_app/entity/CategoryEntity.java
@@ -0,0 +1,30 @@
+package com.grocery.grocery_web_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder @ToString
+@Entity
+@Table(name = "CATEGORY")
+public class CategoryEntity {
+	@Column(name = "CATEGORY_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int categoryId;
+
+	@Column(name = "CATEGORY_NAME")
+	private String categoryName;
+
+	@Column(name = "CATEGORY_VISIBILE")
+	private String categoryVisible;
+}
