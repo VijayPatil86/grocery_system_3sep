dcd561159303cf936687d74590ee2ab533c8b15f
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 14:18:12 +0530

    initial commit

--- src/main/java/com/grocery/grocery_web_app/service/CategoryService.java
+++ src/main/java/com/grocery/grocery_web_app/service/CategoryService.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.grocery.grocery_web_app.bean.CategoryBean;
+import com.grocery.grocery_web_app.bean.DairyProductBean;
 import com.grocery.grocery_web_app.entity.CategoryEntity;
 import com.grocery.grocery_web_app.exception.CategoryNotAvailableException;
+import com.grocery.grocery_web_app.exception.ProductForCategoryNotAvailableException;
 import com.grocery.grocery_web_app.repository.CategoryRepository;
 
 @Service
@@ -34,11 +36,37 @@ public class CategoryService {
 		List<CategoryBean> allAvailableCategoriesBeans = allAvailableCategoriesEntities.stream()
 				.map(categoryEntity -> CategoryBean.builder()
 						.categoryId(categoryEntity.getCategoryId())
-						.categoryName(categoryEntity.getCategoryName()).build())
+						.categoryName(categoryEntity.getCategoryName())
+						.build())
 				.collect(Collectors.toList());
 		LOGGER.info("List<CategoryBean> CategoryService.getAllAvailableCategories() --- available Categories beans are: " +
 				allAvailableCategoriesBeans);
 		LOGGER.debug("List<CategoryBean> CategoryService.getAllAvailableCategories() --- END");
 		return allAvailableCategoriesBeans;
+	}
+
+	public CategoryBean getDairyProducts(int categoryId) {
+		LOGGER.debug("CategoryService.getDairyProducts(" + categoryId + ") --- START");
+		CategoryEntity categoryEntity = categoryRepository.findDairyProductByCategoryId(categoryId);
+		if(categoryEntity == null || categoryEntity.getDairyProductEntities() == null || categoryEntity.getDairyProductEntities().size() == 0) {
+			LOGGER.debug("Dairy products for category " + categoryId + " not available");
+			throw new ProductForCategoryNotAvailableException();
+		}
+		LOGGER.info("CategoryService.getDairyProducts(" + categoryId + ") --- Dairy products entities are," + categoryEntity);
+		List<DairyProductBean> diaryProductBeans = categoryEntity.getDairyProductEntities().stream()
+				.map(dairyProductEntity -> DairyProductBean.builder()
+						.dairyProductId(dairyProductEntity.getDairyProductId())
+						.dairyProductName(dairyProductEntity.getDairyProductName())
+						.build())
+				.collect(Collectors.toList());
+		LOGGER.info("CategoryService.getDairyProducts(" + categoryId + ") --- Dairy products beans are," + diaryProductBeans);
+		CategoryBean categoryBean = CategoryBean.builder()
+				.categoryId(categoryEntity.getCategoryId())
+				.categoryName(categoryEntity.getCategoryName())
+				.dairyProductBeans(diaryProductBeans)
+				.build();
+		LOGGER.info("CategoryService.getDairyProducts(" + categoryId + ") --- Category bean with Dairy product is," + categoryBean);
+		LOGGER.debug("CategoryService.getDairyProducts(" + categoryId + ") --- END");
+		return categoryBean;
 	}
 }
