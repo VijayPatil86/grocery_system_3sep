c2c09ff6c8cf97715922af4891721cccd8b279db
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 07:20:50 +0530

    method name, exception name changed

--- src/main/java/com/grocery/grocery_web_app/controller/CategoryController.java
+++ src/main/java/com/grocery/grocery_web_app/controller/CategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grocery.grocery_web_app.bean.CategoryBean;
-import com.grocery.grocery_web_app.exception.CategoryException;
+import com.grocery.grocery_web_app.exception.CategoryNotAvailableException;
 import com.grocery.grocery_web_app.service.CategoryService;
 
 @RestController
@@ -24,18 +24,18 @@ public class CategoryController {
 	private CategoryService categoryService;
 
 	@GetMapping(path = "/categories")
-	public ResponseEntity<List<CategoryBean>> getAllVisibleCategories() {
-		LOGGER.debug("List<CategoryBean> CategoryController.getAllVisiblecategories() --- START");
-		List<CategoryBean> allVisibleCategoriesBeans = categoryService.getAllVisibleCategories();
-		LOGGER.info("List<CategoryBean> CategoryController.getAllVisiblecategories() --- visible Categories beans are: " +
-				allVisibleCategoriesBeans);
-		LOGGER.debug("List<CategoryBean> CategoryController.getAllVisiblecategories() --- END");
-		return ResponseEntity.ok(allVisibleCategoriesBeans);
+	public ResponseEntity<List<CategoryBean>> getAllAvailableCategories() {
+		LOGGER.debug("List<CategoryBean> CategoryController.getAllAvailableCategories() --- START");
+		List<CategoryBean> allAvailableCategoriesBeans = categoryService.getAllAvailableCategories();
+		LOGGER.info("List<CategoryBean> CategoryController.getAllAvailableCategories() --- available Categories beans are: " +
+				allAvailableCategoriesBeans);
+		LOGGER.debug("List<CategoryBean> CategoryController.getAllAvailableCategories() --- END");
+		return ResponseEntity.ok(allAvailableCategoriesBeans);
 	}
 
-	@ExceptionHandler(value = {CategoryException.class})
-	public ResponseEntity<Void> handleAllVisibleCategoriesNotAvailableException() {
-		LOGGER.debug("ResponseEntity<Void> CategoryController.handleAllVisibleCategoriesNotAvailableException() --- Returning No Content 204");
+	@ExceptionHandler(value = {CategoryNotAvailableException.class})
+	public ResponseEntity<Void> handleCategoryNotAvailableException() {
+		LOGGER.debug("ResponseEntity<Void> CategoryController.handleCategoryNotAvailableException() --- Returning No Content 204");
 		return ResponseEntity.noContent().build();
 	}
 }
