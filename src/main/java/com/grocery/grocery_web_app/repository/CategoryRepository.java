f06a347c31551ec6874f4bf1933cb3dd2eb88ab7
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 11:57:08 +0530

    added method to get available products for given category

--- src/main/java/com/grocery/grocery_web_app/repository/CategoryRepository.java
+++ src/main/java/com/grocery/grocery_web_app/repository/CategoryRepository.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.grocery.grocery_web_app.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
 	@Query(value = "SELECT c FROM CategoryEntity c WHERE c.categoryAvailable = 'Y'")
 	List<CategoryEntity> getAllAvailableCategories();
+
+	@Query("SELECT c FROM CategoryEntity c LEFT JOIN FETCH c.dairyProductEntities d "
+		       + "WHERE c.categoryId = :categoryId "
+		       + "AND c.categoryAvailable = 'Y' "
+		       + "AND d.dairyProductAvailable = 'Y'")
+	CategoryEntity findDairyProductByCategoryId(@Param(value = "categoryId") int categoryId);
 }
