226f6aeea3e515143aa2ee7da42c99af2c971863
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 11:53:59 +0530

    added changes related to retrieve products for given category

--- src/main/java/com/grocery/grocery_web_app/controller/CategoryController.java
+++ src/main/java/com/grocery/grocery_web_app/controller/CategoryController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grocery.grocery_web_app.bean.CategoryBean;
 import com.grocery.grocery_web_app.exception.CategoryNotAvailableException;
+import com.grocery.grocery_web_app.exception.ProductForCategoryNotAvailableException;
 import com.grocery.grocery_web_app.service.CategoryService;
 
 @RestController
@@ -23,19 +25,39 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
-	@GetMapping(path = "/categories")
+	@GetMapping(path = "/category")
 	public ResponseEntity<List<CategoryBean>> getAllAvailableCategories() {
-		LOGGER.debug("List<CategoryBean> CategoryController.getAllAvailableCategories() --- START");
+		LOGGER.debug("/api/category --- START");
+		LOGGER.debug("CategoryController.getAllAvailableCategories() --- START");
 		List<CategoryBean> allAvailableCategoriesBeans = categoryService.getAllAvailableCategories();
 		LOGGER.info("List<CategoryBean> CategoryController.getAllAvailableCategories() --- available Categories beans are: " +
 				allAvailableCategoriesBeans);
-		LOGGER.debug("List<CategoryBean> CategoryController.getAllAvailableCategories() --- END");
+		LOGGER.debug("CategoryController.getAllAvailableCategories() --- END");
+		LOGGER.debug("/api/category --- END");
 		return ResponseEntity.ok(allAvailableCategoriesBeans);
+	}
+
+	@GetMapping(path = "/category/{categoryId}")
+	public ResponseEntity<Object> getProductById(@PathVariable(name = "categoryId") int categoryId){
+		LOGGER.debug("/api/category/" + categoryId + " --- START");
+		LOGGER.debug("CategoryController.getProductById(" + categoryId + ") --- START");
+		LOGGER.info("CategoryController.getProductById(" + categoryId + ") --- retrieving products, ");
+		CategoryBean categoryBean = categoryService.getDairyProducts(categoryId);
+		LOGGER.info("CategoryController.getProductById(" + categoryId + ") --- products are, " + categoryBean);
+		LOGGER.debug("CategoryController.getProductById(" + categoryId + ") --- END");
+		LOGGER.debug("/api/category/" + categoryId + " --- END");
+		return ResponseEntity.ok(categoryBean);
 	}
 
 	@ExceptionHandler(value = {CategoryNotAvailableException.class})
 	public ResponseEntity<Void> handleCategoryNotAvailableException() {
 		LOGGER.debug("ResponseEntity<Void> CategoryController.handleCategoryNotAvailableException() --- Returning No Content 204");
+		return ResponseEntity.noContent().build();
+	}
+
+	@ExceptionHandler(value = {ProductForCategoryNotAvailableException.class})
+	public ResponseEntity<Void> handleProductForcategoryNotAvailableException() {
+		LOGGER.debug("ResponseEntity<Void> CategoryController.handleProductForcategoryNotAvailableException() --- Returning No Content 204");
 		return ResponseEntity.noContent().build();
 	}
 }
