eb04b9d9d6a28df863b40623dc804f0f01157497
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 11:55:18 +0530

    added list field for DairyProductEntity

--- src/main/java/com/grocery/grocery_web_app/entity/CategoryEntity.java
+++ src/main/java/com/grocery/grocery_web_app/entity/CategoryEntity.java
@@ -1,10 +1,14 @@
 package com.grocery.grocery_web_app.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,4 +32,7 @@ public class CategoryEntity {
 	@Column(name = "CATEGORY_AVAILABLE")
 	private String categoryAvailable;
 
+	@OneToMany
+	@JoinColumn(name = "CATEGORY_ID")
+	private List<DairyProductEntity> dairyProductEntities;
 }
