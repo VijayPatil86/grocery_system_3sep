8254e00d0108c2a14bc93f10bcf985778f2d81af
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-09 12:16:24 +0530

    extended class to implement HATEOAS

--- src/main/java/com/grocery/grocery_web_app/bean/CategoryBean.java
+++ src/main/java/com/grocery/grocery_web_app/bean/CategoryBean.java
@@ -2,13 +2,15 @@ package com.grocery.grocery_web_app.bean;
 
 import java.util.List;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @Builder @ToString
-public class CategoryBean {
+public class CategoryBean extends RepresentationModel<CategoryBean> {
 	private int categoryId;
 	private String categoryName;
 	private List<DairyProductBean> dairyProductBeans;
